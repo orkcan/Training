684836520889574b19f15a778fe2ee7cff1b052d
orkcan <yusuforkcan@gmail.com>
2024-02-02 14:19:17 +0100

    commit and push

--- django_offer_practice/products/urls.py
+++ django_offer_practice/products/urls.py
@@ -1,2 +1,10 @@
-from django.conf.urls import path,include
+from django.conf.urls import path, include
+from . import views
 
+#/products/1/detail
+#/products/new
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('another_example/', views.another_example_view),
+]
